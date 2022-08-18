78e46b8a239fdb611555a2100b5dea804911b9d6
Sin <102574135+yusin07@users.noreply.github.com>
2022-08-18 12:27:11 +0800

    first commit

---
+++ app/assets/js/swiper.js
@@ -0,0 +1,23 @@
+//   // core version + navigation, pagination modules:
+//   import Swiper, { Navigation, Pagination } from 'swiper';
+//   // import Swiper and modules styles
+
+//   // init Swiper:
+//   const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     loop: true,
+//     grabCursor : true,
+  
+//     // If we need pagination
+//     pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//     },
+  
+//   });
